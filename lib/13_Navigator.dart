a13656adf24887574ac47172c95c8a38c426803b
gitminsoo <dwn6629@naver.com>
2023-03-21 12:20:21 +0900

    Navigator study (navigtor.push&pop)

---
+++ lib/13_Navigator.dart
@@ -0,0 +1,68 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_first_make/1_main.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navigator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.deepOrange),
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context2) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go to the Second page'),
+          onPressed: () {
+            Navigator.push(
+                context2,
+                MaterialPageRoute(
+                  builder: (context) => SecondPage(),
+                ));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext ctx) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go to the First page'),
+          onPressed: () {
+            Navigator.pop(ctx);
+            // 자신이 사라지기만 하면 되어서 굉장히 간단함
+          },
+        ),
+      ),
+    );
+    ;
+  }
+}
